77fecbeccfc65905ed5d214b6be526f6c85bafcf
Юлия <juliasaenko@MacBook-Pro-Ulia.local>
2020-05-11 12:03:13 +0300

    local commit

--- src/tasks/week-8/components/AuthorizedNavbar.js
+++ src/tasks/week-8/components/AuthorizedNavbar.js
@@ -10,6 +10,7 @@ function AuthorizedNavbar() {
     <Menu>
       <NavLink to='/posts' className='item' activeClassName='active-nav'>Posts</NavLink>
       <NavLink to='/users' className='item' activeClassName='active-nav'>Users</NavLink>
+      <NavLink to='/friends' className='item' activeClassName='active-nav'>Friends</NavLink>
       <Menu.Menu position='right'>
         <Menu.Item onClick={() => dispatch(logout())}>Logout</Menu.Item>
       </Menu.Menu>
--- src/tasks/week-8/features/posts/containers/PostsPage.js
+++ src/tasks/week-8/features/posts/containers/PostsPage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../slices/postsSlice';
+import Post from '../components/post';
 
-function PostsPage(props) {
+function PostsPage() {
+  const dispatch = useDispatch();
+  const { isLoading, posts } = useSelector(state => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts())
+  },[dispatch]);
+
+
   return (
-    <div>Posts Page Will be there</div>
+      <ul>
+        {posts.map(post => <Post key={post._id} post={post} />)}
+      </ul>
   );
 }
+
 
 export default PostsPage;
